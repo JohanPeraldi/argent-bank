884042e8d8fab56885f448dd4950c1217769b492
Johan Peraldi <johan.peraldi@gmail.com>
2023-03-08 20:40:22 +0200

    Replace NavLink with Link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,30 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../../img/argentBankLogo.png';
 import styles from './Navigation.module.css';
 
 export default function Navigation(props) {
   return (
     <nav className={styles['main-nav']}>
-      <NavLink to="/" className={styles['main-nav-logo']}>
+      <Link to="/" className={styles['main-nav-logo']}>
         <img
           className={styles['main-nav-logo-image']}
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </NavLink>
+      </Link>
       <div>
         {!props.loggedIn && (
-          <NavLink to="sign-in" className={styles['main-nav-item']}>
+          <Link to="sign-in" className={styles['main-nav-item']}>
             <i className="fa fa-user-circle"></i>
             Sign In
-          </NavLink>
+          </Link>
         )}
         {props.loggedIn && (
-          <NavLink to="/" className={styles['main-nav-item']}>
+          <Link to="/" className={styles['main-nav-item']}>
             <i class="fa fa-sign-out"></i>
             Sign Out
-          </NavLink>
+          </Link>
         )}
       </div>
     </nav>
