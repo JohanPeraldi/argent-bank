ec1a6449d734d5ad1030f8b94206a54a801ec7c0
Johan Peraldi <johan.peraldi@gmail.com>
2023-03-16 19:31:25 +0200

    Add basic input validation to disallow form submission with empty fields

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,36 +6,82 @@ import styles from './LoginForm.module.css';
 export default function LoginForm() {
   const loggedIn = useSelector((state) => state.login.loggedIn);
   // const dispatch = useDispatch();
+  // Username and password values
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  // Validity of username and password values
+  // (only reject empty fields, but may be further restricted)
+  const [usernameIsValid, setUsernameIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+  // Have input fields been entered and exited by user (blur event)?
+  const [usernameInputEntered, setUsernameInputEntered] = useState(false);
+  const [passwordInputEntered, setPasswordInputEntered] = useState(false);
+  // Dynamic className depending on both input validity
+  // and input field being entered and left (blur event)
+  const usernameInputClassName =
+    usernameIsValid || !usernameInputEntered ? '' : 'input-invalid';
+  const passwordInputClassName =
+    passwordIsValid || !passwordInputEntered ? '' : 'input-invalid';
+  // Form validity (false by default, true if both inputs are valid)
+  let formIsValid = false;
+  if (usernameIsValid && passwordIsValid) {
+    formIsValid = true;
+  }
+  // Event handler functions
   const inputChangeHandler = (event) => {
+    if (event.target.value.trim() === '') return;
     if (event.target.type === 'text') {
+      setUsernameIsValid(true);
       setUsername(event.target.value);
     }
     if (event.target.type === 'password') {
+      setPasswordIsValid(true);
       setPassword(event.target.value);
+    }
+  };
+  const inputBlurHandler = (event) => {
+    if (event.target.value.trim() === '') {
+      if (event.target.type === 'text') {
+        setUsernameIsValid(false);
+        setUsernameInputEntered(true);
+      }
+      if (event.target.type === 'password') {
+        setPasswordIsValid(false);
+        setPasswordInputEntered(true);
+      }
+      event.target.placeholder = 'Field cannot be empty!';
     }
   };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-
-    if (username.trim() === '' || password.trim() === '') {
-      return;
-    }
-
-    console.log('Username: ', username);
-    console.log('Password: ', password);
+    if (formIsValid) {
+      // submit form
+      console.log('Username: ', username);
+      console.log('Password: ', password);
+    } else return;
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={styles['input-wrapper']}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" onChange={inputChangeHandler} />
+        <input
+          type="text"
+          id="username"
+          className={styles[usernameInputClassName]}
+          onBlur={inputBlurHandler}
+          onChange={inputChangeHandler}
+        />
       </div>
       <div className={styles['input-wrapper']}>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" onChange={inputChangeHandler} />
+        <input
+          type="password"
+          id="password"
+          className={styles[passwordInputClassName]}
+          onBlur={inputBlurHandler}
+          onChange={inputChangeHandler}
+        />
       </div>
       <div className={styles['input-remember']}>
         <input type="checkbox" id="remember-me" />
@@ -44,6 +90,7 @@ export default function LoginForm() {
       {!loggedIn && (
         <button
           className={styles['sign-in-button']}
+          disabled={!formIsValid}
           // onClick={() => dispatch(login())}
         >
           Sign In
