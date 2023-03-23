8ad8518d379a7e05add87342397a17ec03342b20
Johan Peraldi <johan.peraldi@gmail.com>
2023-03-23 18:21:54 +0200

    Remove unnecessary code & attempt authentication check on Profile page by adding loader

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,6 +1,4 @@
 import { useEffect } from 'react';
-import { redirect } from 'react-router-dom';
-import axios from 'axios';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import styles from './Login.module.css';
 
@@ -19,39 +17,4 @@ export default function Login() {
       </section>
     </main>
   );
-}
-
-export async function action({ request }) {
-  const data = await request.formData();
-  const authenticationData = {
-    username: data.get('username'),
-    password: data.get('password'),
-  };
-  const port = '3001';
-  const baseUrl = `http://localhost:${port}/api/v1`;
-  const body = JSON.stringify(authenticationData);
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: `${baseUrl}/user/login`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: body,
-  };
-
-  axios.request(config).then((response) => {
-    console.log(JSON.stringify(response.data));
-    if (response.status === 400) {
-      // Invalid fields
-    }
-    if (response.status === 500) {
-      // Internal server error
-    }
-    if (response.status === 200) {
-      // TODO: Manage token
-
-      return redirect('/profile');
-    }
-  });
 }
