33325449079f37d7e8a3c55b4cc17c6ddc312782
Johan Peraldi <johan.peraldi@gmail.com>
2023-04-14 21:32:44 +0300

    Move static data relating to features into new file

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Feature from './components/Feature/Feature';
 import Hero from './components/Hero/Hero';
+import { features } from './data/features';
 import { fetchUser } from './features/login/loginSlice';
-import chatIcon from './img/icon-chat.png';
-import moneyIcon from './img/icon-money.png';
-import securityIcon from './img/icon-security.png';
 import styles from './App.module.css';
 
 function App() {
@@ -23,36 +21,18 @@ function App() {
     }
   }, [dispatch, loggedIn]);
 
-  const firstFeatureText =
-    'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.';
-  const firstFeatureTitle = 'You are our #1 priority';
-  const chatIconAltText = 'Chat icon';
-  const secondFeatureText =
-    'The more you save with us, the higher your interest rate will be!';
-  const secondFeatureTitle = 'More savings means higher rates';
-  const moneyIconAltText = 'Money icon';
-  const thirdFeatureText =
-    'We use top of the line encryption to make sure your data and money is always safe.';
-  const thirdFeatureTitle = 'Security you can trust';
-  const securityIconAltText = 'Security icon';
-  const data = [
-    [firstFeatureTitle, firstFeatureText, chatIcon, chatIconAltText, 1],
-    [secondFeatureTitle, secondFeatureText, moneyIcon, moneyIconAltText, 2],
-    [thirdFeatureTitle, thirdFeatureText, securityIcon, securityIconAltText, 3],
-  ];
-
   return (
     <main className="main">
       <Hero />
       <section className={styles.features}>
         <h2 className="sr-only">Features</h2>
-        {data.map((feature) => (
+        {features.map((feature) => (
           <Feature
-            title={feature[0]}
-            text={feature[1]}
-            image={feature[2]}
-            imageAltText={feature[3]}
-            key={feature[4]}
+            title={feature.title}
+            text={feature.text}
+            image={feature.image}
+            imageAltText={feature.imageAltText}
+            key={feature.key}
           />
         ))}
       </section>
---
+++ src/data/features.js
@@ -0,0 +1,27 @@
+import chatIcon from '../img/icon-chat.png';
+import moneyIcon from '../img/icon-money.png';
+import securityIcon from '../img/icon-security.png';
+
+export const features = [
+  {
+    title: 'You are our #1 priority',
+    text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    image: chatIcon,
+    imageAltText: 'Chat icon',
+    key: 1,
+  },
+  {
+    title: 'More savings means higher rates',
+    text: 'The more you save with us, the higher your interest rate will be!',
+    image: moneyIcon,
+    imageAltText: 'Money icon',
+    key: 2,
+  },
+  {
+    title: 'Security you can trust',
+    text: 'We use top of the line encryption to make sure your data and money is always safe.',
+    image: securityIcon,
+    imageAltText: 'Security icon',
+    key: 3,
+  },
+];
